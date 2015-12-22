71e85209eb29492d7dbbfe92a2ab6804427e9f08
Azim <djuraeva@pattonvillecs.com>
2015-12-22 10:23:50 -0600

    Added buttons

---
+++ app/src/main/java/com/example/yeshiwak/pittapp/MainActivityFragment.java
@@ -0,0 +1,18 @@
+package com.example.yeshiwak.pittapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.widget.Button;
+
+/**
+ * Created by djuraeva on 12/22/15.
+ */
+public class MainActivityFragment extends Fragment {
+    private Button 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
