eb0e15824146ee227e10bbe48c9b30c0d6dec325
Azim <djuraeva@pattonvillecs.com>
2016-02-23 09:45:54 -0600

    Added buttons

--- app/src/main/java/com/example/yeshiwak/pittapp/ScanActivity.java
+++ app/src/main/java/com/example/yeshiwak/pittapp/ScanActivity.java
@@ -1,21 +1,60 @@
 package com.example.yeshiwak.pittapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class ScanActivity extends ActionBarActivity {
+    private Button mScanButton;
+    private final int REQUEST_QR = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        mScanButton = (Button) findViewById(R.id.qr_button);
+
+        mScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+                    startActivityForResult(intent, REQUEST_QR);
+                } catch (Exception e) {
+                    Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+                    Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+                    startActivity(marketIntent);
+                }
+
+            }
+        });
+
     }
 
 
-    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_QR)
+            if (resultCode == Activity.RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                String format = data.getStringExtra("SCAN_RESULT_FORMAT");
+                Toast.makeText(getApplicationContext(), contents, Toast.LENGTH_SHORT).show();
+                // TODO: Do something here with it
+            }
+
+    }
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_scan, menu);
