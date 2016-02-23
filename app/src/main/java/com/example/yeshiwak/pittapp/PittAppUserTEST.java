10c30bebc369b1042b7c8d69fb653bb66b99bf5a
Kalekidan <kalekidan98@gmail.com>
2016-02-23 09:47:28 -0600

    commented out

--- app/src/main/java/com/example/yeshiwak/pittapp/PittAppUserTEST.java
+++ app/src/main/java/com/example/yeshiwak/pittapp/PittAppUserTEST.java
@@ -1,11 +1,17 @@
+import android.app.Application;
+
+/*import android.app.Application;
+
+import android.app.Application;
+
 package com.example.yeshiwak.pittapp;
 
 import android.app.Application;
 import com.parse.Parse;
-/**
+/*
  * Created by yeshiwak on 2/22/16.
  */
-public class PittAppUserTEST extends Application {
+/*public class PittAppUserTEST extends Application {
 
         private final String APPLICATION_ID = "";
         private final String CLIENT_KEY = "";
@@ -16,12 +22,15 @@ public class PittAppUserTEST extends Application {
 
             Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
 
-/*        ParseObject testObject = new ParseObject("TestObject");
+/       ParseObject testObject = new ParseObject("TestObject");
         testObject.put("message", "hello world!");
         testObject.saveInBackground();
-*/
+
         }
 
     }
 
-}
+*/
+
+
+
